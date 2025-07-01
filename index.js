66908614e4fd33985e4bbc0b1b0c2287c7ab8b65
Patiphan Phengpao <misterpatipan123@gmail.com>
2025-07-01 23:48:19 +0700

    Add new /api/info/ endpoint

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
     res.send('Hello, World from Railway!');
 })
 
+app.get('/api/info', (req, res) => {
+    res.json({ message: 'This is a new endpoint!' });
+})
+
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
 })
